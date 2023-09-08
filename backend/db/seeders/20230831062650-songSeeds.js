1b1b4c65a810e34fe110c38bfd6ca5e65d772b77
Kevin Kim <kebonkim@gmail.com>
2023-09-07 23:43:52 -0700

    song seeds added

--- backend/db/seeders/20230831062650-songSeeds.js
+++ backend/db/seeders/20230831062650-songSeeds.js
@@ -1,25 +1,40 @@
 'use strict';
 
 /** @type {import('sequelize-cli').Migration} */
+
+const songArr = [
+  {
+    userId: 1,
+    albumId: 1,
+    title: "You're Not You Anymore",
+    description: "Metal",
+    url: "music.com",
+    imageUrl: "image.com",
+  },
+  {
+    userId: 2,
+    albumId: 2,
+    title: "Nothing Left To Love",
+    description: "Metal",
+    url: "music.com",
+    imageUrl: "image.com",
+  },
+  {
+    userId: 3,
+    albumId: 3,
+    title: "Whispers of Your Death",
+    description: "Metal",
+    url: "music.com",
+    imageUrl: "image.com",
+  },
+];
+
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    await queryInterface.bulkInsert('Songs', )
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    await queryInterface.bulkDelete('Songs', )
   }
 };
