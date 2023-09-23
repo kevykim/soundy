54ad93acb3c21f187fc6e34505d4c2808300a11b
Kevin Kim <kebonkim@gmail.com>
2023-09-22 23:08:59 -0700

    API route add song to playlist based on playlist id

--- backend/routes/api/playlists.js
+++ backend/routes/api/playlists.js
@@ -5,7 +5,7 @@ const { handleValidationErrors } = require("../../utils/validation");
 const { requireAuth } = require("../../utils/auth");
 const { Op } = require("sequelize");
 
-const { Song, User, Playlist } = require("../../db/models");
+const { Song, User, Playlist, PlaylistSong } = require("../../db/models");
 
 
 const playlistValidation = [
@@ -27,6 +27,61 @@ router.post('/', requireAuth, playlistValidation, async (req, res) => {
 
     res.status(201);
     return res.json({createSong});
+
+});
+
+
+
+// Add a song to playlist based on Playlist's id
+router.post('/:playlistId/songs', requireAuth, async (req, res, next) => {
+
+    const {songId} = req.body;
+
+    const playlistId = req.params.playlistId;
+    const userId = req.user.id;
+
+
+    const findPlaylist = await Playlist.findByPk(playlistId);
+    const findSong = await Song.findByPk(songId);
+
+    if (!findPlaylist) {
+      const err = new Error("Couldn't find a Playlist with the specified id");
+      err.title = "Playlist couldn't be found";
+      err.errors = "Playlist couldn't be found";
+      err.status = 404;
+      return next(err);
+    }
+
+    if (!findSong) {
+      const err = new Error("Couldn't find a Song with the specified id");
+      err.title = "Song couldn't be found";
+      err.errors = "Song couldn't be found";
+      err.status = 404;
+      return next(err);
+    }
+
+    const addToPlaylist = await PlaylistSong.create({
+        playlistId : playlistId,
+        songId,
+    });
+    
+    if (findPlaylist.userId === userId) {
+        res.status(200);
+        return res.json(addToPlaylist)
+    } else {
+        const err = new Error("Must be owner of Playlist")
+        err.title = 'Must be owner'
+        err.errors = "Must be owner"
+        err.status = 403;
+        return next(err)
+    }
+      
+
+
+    
+
+
+
 
 });
 
