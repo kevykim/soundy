8e6bdded842515e33720923ff5c3bfe0e7469715
Kevin Kim <kebonkim@gmail.com>
2023-09-26 09:39:10 -0700

    API ROUTES: create,edit,and delete album

--- backend/routes/api/albums.js
+++ backend/routes/api/albums.js
@@ -9,6 +9,18 @@ const { Op } = require("sequelize");
 const { Song, User, Comment, Playlist, PlaylistSong, Album } = require("../../db/models");
 
 
+const validateAlbum = [
+  check("title")
+    .exists({ checkFalsy: true })
+    .withMessage("Album title is required"),
+  check("description")
+    .exists({ checkFalsy: true })
+    .withMessage("Album description is required"),
+  check("year")
+    .exists({ checkFalsy: true })
+    .withMessage("Album year is required"),
+    handleValidationErrors
+];
 
 
 // GET all current user Albums
@@ -23,6 +35,46 @@ router.get('/current', async (req, res, next) => {
     res.status(200);   
     return res.json({Albums : currentAlbum});
     
+
+});
+
+
+// EDIT an Album
+router.put('/:albumId', requireAuth, validateAlbum, async (req, res, next) => {
+
+    const userId = req.user.id;
+
+    const { title, description, imageUrl, year } = req.body;
+
+    const albumId = req.params.albumId;
+
+    const editAlbum = await Album.findOne({ where : {
+            [Op.and] : [
+                {userId : userId},
+                {id : albumId}
+            ]
+        }
+    });
+
+    if (editAlbum) {
+        await editAlbum.update({
+            userId,
+            title, 
+            description,
+            year, 
+            imageUrl : imageUrl || "replaceimage.com"
+        });
+
+        res.status(200);
+        return res.json(editAlbum);
+    } else {
+        const err = new Error("Couldn't find an Album with the specified id");
+        err.title = "Album couldn't be found";
+        err.errors = "Album couldn't be found";
+        err.status = 404;
+        return next(err);
+    }
+
 
 });
 
@@ -64,6 +116,65 @@ router.get('/', async (req, res, next) => {
     return res.json({Albums : findAlbums});
 
 }); 
+
+
+// Create an Album
+router.post('/', requireAuth, validateAlbum, async (req, res, next) => {
+
+    const userId = req.user.id;
+
+    const { title, description, imageUrl, year } = req.body;
+
+    const createAlbum = await Album.create({
+        userId : userId,
+        title,
+        description,
+        year,
+        imageUrl : imageUrl || "replaceimage.com"
+    });
+
+    res.status(201);
+    return res.json(createAlbum);
+
+
+});
+
+
+
+// DELETE an Album
+router.delete('/:albumId', requireAuth, async (req, res, next) => {
+
+    const albumId = req.params.albumId;
+
+    const userId = req.user.id;
+
+    const deleteAlbum = await Album.findOne({
+         where : {
+            [Op.and] : [
+                {id : albumId},
+                {userId : userId}
+            ]
+        }
+    })
+
+    if (deleteAlbum) {
+
+        await deleteAlbum.destroy();
+
+        res.status(200);
+        return res.json({
+            message : "Successfully deleted",
+            statusCode : 200
+        });
+    } else {
+        const err = new Error("Couldn't find an Album with the specified id");
+        err.title = "Album couldn't be found";
+        err.errors = "Album couldn't be found";
+        err.status = 404;
+        return next(err);
+    };
+
+});
 
 
 
