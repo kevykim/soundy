bcbdbc0fcb8a1964ce54d06d508f5aa8f8b41923
Kevin Kim <kebonkim@gmail.com>
2023-09-26 09:38:35 -0700

    API route artist detail from ID

--- backend/routes/api/artists.js
+++ backend/routes/api/artists.js
@@ -5,6 +5,31 @@ const { Song, User, Playlist, Album } = require('../../db/models');
 
 
 
+// GET details of an Artist from ID
+router.get('/:artistId', async (req, res, next) => {
+
+    const artistId = req.params.artistId;
+
+    let users = await User.findByPk(artistId);
+
+     if (!users) {
+       const err = new Error("Couldn't find an Artist with the specified id");
+       err.message = "Artist couldn't be found";
+       err.errors = ["Couldn't find an Artist with the specified id"];
+       err.status = 404;
+       return next(err);
+     }
+
+     users = users.toJSON();
+     users.totalSongs = await Song.count({ where: { userId : artistId } });
+     users.totalAlbums = await Album.count({ where: { userId : artistId } });
+
+     res.status(200);
+     return res.json(users)
+
+});
+
+
 // Get All Songs of Artist from ID
 router.get('/:artistId/songs', async (req, res, next) => {
 
