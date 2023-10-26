851ab38042a0d60175543dc682ef0f492d309822
Kevin Kim <kebonkim@gmail.com>
2023-10-25 23:48:38 -0700

    seeds and updated readme

--- backend/db/seeders/20230831062650-songSeeds.js
+++ backend/db/seeders/20230831062650-songSeeds.js
@@ -30,6 +30,60 @@ const songArr = [
     imageUrl:
       "https://images.pexels.com/photos/1713953/pexels-photo-1713953.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
   },
+  {
+    userId: 1,
+    albumId: 1,
+    title: "Fragile Limbs",
+    description: "Metal",
+    url: "music.com",
+    imageUrl:
+      "https://images.pexels.com/photos/1713953/pexels-photo-1713953.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    userId: 2,
+    albumId: 2,
+    title: "Ocean of Another",
+    description: "Metal",
+    url: "music.com",
+    imageUrl:
+      "https://images.pexels.com/photos/1713953/pexels-photo-1713953.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    userId: 3,
+    albumId: 3,
+    title: "Skin Beneath A Scar",
+    description: "Metal",
+    url: "music.com",
+    imageUrl:
+      "https://images.pexels.com/photos/1713953/pexels-photo-1713953.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    userId: 1,
+    albumId: 1,
+    title: "Bouquet",
+    description: "Metal",
+    url: "music.com",
+    imageUrl:
+      "https://images.pexels.com/photos/1713953/pexels-photo-1713953.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    userId: 2,
+    albumId: 2,
+    title: "Paradise and Plague",
+    description: "Metal",
+    url: "music.com",
+    imageUrl:
+      "https://images.pexels.com/photos/1713953/pexels-photo-1713953.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    userId: 3,
+    albumId: 3,
+    title: "What Mirrors Might Reflect",
+    description: "Metal",
+    url: "music.com",
+    imageUrl:
+      "https://images.pexels.com/photos/1713953/pexels-photo-1713953.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
 ];
 
 module.exports = {
--- readme.md
+++ readme.md
@@ -1,5 +1,27 @@
-SoundCloud Clone
+# About Soundy
 
-Backend: Express, Sequelize, Postgres
-Frontend: React, Redux, TailWind, Next.js
-Deployment: Render
+Soundy is a soundcloud cloud web applicaiton that allows users to listen music, upload songs, and be able to leave comments on their favorite track. 
+
+<img align='center' src="./frontend/src/public/assets/Soundy.png" />
+
+### Backend: Express, Sequelize, Postgres
+### Frontend: React, Redux, TailWind
+### Deployment: Render
+### Build: Vite
+
+# Run Locally
+
+1. Clone this repository
+2. `cd` into the cloned file.
+3. `npm install` to install dependencies in both frontend and backend directory.
+4. Create a `.env` file using the `.env.example` as a guide.
+    ```
+    PORT=
+    DB_FILE=db/dev.db
+    JWT_SECRET=
+    JWT_EXPIRES_IN=
+    ```
+5. Move to backend directory.
+6. Run `npx dotenv sequelize db:migrate` 
+7. Run `npx dotenv sequelize db:seed:all`
+8. With two terminals, Run `npm start` in both frontend and backend directory to start the application.
